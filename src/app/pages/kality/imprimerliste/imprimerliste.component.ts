e1e412c610c273bbbe75d665c6e81a2011a82422
MatsuKaki <dang_kim_moana@yahoo.fr>
2021-02-05 12:16:42 +0100

    1 file modified, 1 deleted

--- src/app/pages/kality/imprimerliste/imprimerliste.component.ts
+++ src/app/pages/kality/imprimerliste/imprimerliste.component.ts
@@ -7,7 +7,10 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ["./imprimerliste.component.scss"]
 })
 export class ImprimerListeComponent {
-  pdfSource = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+  pdfSource =
+    "https://github.com/MatsuKaki/ionic-v4-kality/blob/master/src/app/pages/kality/pictures/Liste%20des%20r%C3%A9serves.pdf";
+  //https://github.com/MatsuKaki/i
+  /*pdfSource = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";*/
 
   constructor() {}
 }
