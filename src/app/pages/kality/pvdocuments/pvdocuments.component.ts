ba209a05b23b256f27ab57393c1e05a57248d191
MatsuKaki <dang_kim_moana@yahoo.fr>
2021-04-21 17:55:50 +0200

    2 files modified

--- src/app/pages/kality/pvdocuments/pvdocuments.component.ts
+++ src/app/pages/kality/pvdocuments/pvdocuments.component.ts
@@ -1,10 +1,41 @@
 import { Component } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "pvdocuments-component",
   templateUrl: "./pvdocuments.component.html"
 })
 export class PVDocumentsComponent {
-  constructor() {}
+  constructor(
+    private alertController: AlertController,
+    private router: Router
+  ) {}
+
+  async presentAlert() {
+    console.log("Print clicked");
+    const alert = await this.alertController.create({
+      cssClass: "my-custom-class",
+      header: "Attention",
+      message: "H1 et H2 ne peuvent pas être sélectionnés en même temps",
+      buttons: [
+        {
+          text: "Annuler",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel: blah");
+          }
+        },
+        {
+          text: "[TEST] Continuer",
+          handler: () => {
+            this.router.navigate(["kality/imprimerpv"]);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
